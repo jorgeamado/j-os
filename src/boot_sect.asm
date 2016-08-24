f299ca73a071750c122dff360ac80f5a5180d085
georgel <george.lemeshko@gmail.com>
2016-08-24 16:06:36 +0300

    Use data segment instead org directive

--- src/boot_sect.asm
+++ src/boot_sect.asm
@@ -1,14 +1,19 @@
-[org 0x7c00]
+;;[org 0x7c00]
+
+	mov bx, 0x7c0 	; set data segment to 0x7c0 to tell CPU
+	mov ds, bx		; use addresses ds*16 == 0x7c00 + [addres]
+					; it will make same thing like [org 0x7c00]
+
 	mov dx,  [NUM]
 	call print_hex
-	
-	jmp $      
 
-	NUM: db 1
+	jmp $
+
+	NUM: dw 0x1234 ;NUM as a pointer to word
 
 	%include "tools/print_str_func.asm"
 	%include 'tools/print_hex_func.asm'
-
+	
 	times 510-($-$$) db 0
 	dw 0xaa55
-
+	
