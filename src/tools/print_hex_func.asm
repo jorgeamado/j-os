984658ed398d920c982724571060edaa6af75a89
georgel <george.lemeshko@gmail.com>
2016-10-01 23:04:14 +0300

    Using zero-symbol for hex printing, to avoid using ruined hex template

--- src/tools/print_hex_func.asm
+++ src/tools/print_hex_func.asm
@@ -4,7 +4,6 @@ print_hex:
 	mov cx, 4			;cx - counter to get 4 bytes 
 	mov bx, HEX_OUT		;put pointer to template "0x0000" to bx
 	add bx, 5			;set bx pointer to the last char of template "0x000 >0"
-	;;add byte [bx], 7	
 
 	charloop:
 	mov ax, dx			;move value from dx to ax
@@ -12,10 +11,12 @@ print_hex:
 
 	cmp ax, 0xa			;MAGIC if this is letter we need to add 7
 	jl to_string
-	add byte [bx], 7
+	add byte al, 7
 
 	to_string:
-	add byte [bx], al 	;change template string add value to char '0' + 2 = '2' 
+	add al, [ZERO_SYMBOL]
+
+	mov byte [bx], al 	;change template string add value to char '0'(zero-symbol) + 2 = '2' 
 
 	shr dx, 4			;shift right dx, to use last byte for printing
 	dec cx				;dec cicle counter
@@ -28,8 +29,12 @@ print_hex:
 	mov bx, HEX_OUT		;put updated template to bx
 	call print_string	;call print string from bx
 
+	mov bx, HEX_OUT		;put pointer to template "0x0000" to bx
+	add bx, 5			;set bx pointer to the last char of template "0x000 >0"
+
 	popa				;restore registers
 	ret
 
 HEX_OUT: db '0x0000',0 	;create template sctring "0x0000" and add to the 
 						;zero byte after the string
+ZERO_SYMBOL: db '0'		; zero-symbol - for string creation				
